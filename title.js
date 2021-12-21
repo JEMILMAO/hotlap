6ee994c70da876714b2d0c36ea0adc25cceeea13
JEMILMAO <65700244+JEMILMAO@users.noreply.github.com>
2021-12-21 01:34:54 -0800

    Update title.js

--- title.js
+++ title.js
@@ -11,7 +11,7 @@ function loaded() {
     setInterval(loop, 250);
 }
 var x = 0;
-var titleText = [ "j", "je", "jem", "jemi", "jemi.", "jemi.g", "jemi.gg", "jemi.g", "jemi.", "jemi", "jem", "je", "j", "." ];
+var titleText = [ "o", "on", "oni", "onik", "oniky", "onikya", "onikyan", "onikyan.", "onikyan.j", "onikyan.jp", "onikyan.j", "onikyan.", "onikyan", "onikya", "oniky", "onik", "oni", "on", "o",];
 
 function loop() {
     document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
